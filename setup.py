1f336fc7c550287407c885e4b9b9486774c4f0da
wojtek <wojtek.semik@gmail.com>
2019-12-09 21:42:52 +0100

    Updated packages.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Build Tools",
         "Operating System :: POSIX :: Linux",
     ],
