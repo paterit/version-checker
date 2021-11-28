84ef6b867bb24523160c2c20b3181e743d6bc01a
Wojciech Semik <wojtek.semik@gmail.com>
2021-11-28 22:46:15 +0100

    Python 3.10 in tox now.

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.7",
